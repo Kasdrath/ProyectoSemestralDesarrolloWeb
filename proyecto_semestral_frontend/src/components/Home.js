f7e58c7bd9a23b1fce1154f2cc69bb9be798b60e
ManuelYanezA <manuel.yanez1501@alumnos.ubiobio.cl>
2023-01-10 12:42:41 -0300

    intento de conexion en frontend a backend

--- proyecto_semestral_frontend/src/components/Home.js
+++ proyecto_semestral_frontend/src/components/Home.js
@@ -1,13 +1,41 @@
 import * as React from 'react';
-import CDCard from './CentroDistribucionCard'
+import {useEffect, useState} from 'react';
+import CDCard from './CentroDistribucionCard';
+import axios from 'axios';
+// eslint-disable-next-line
+//import {Link} from 'react-router-dom';
 
+const endpoint = 'http://127.0.0.1:8000/api'
 
 const Home = () => {
+
+    // const [centroDist, setCentroDist] = useState([])
+    // useEffect ( ()=> {
+    //     getAllCentroDist()
+    // }, [])
+    // const getAllCentroDist = async () => {
+    //     const response = await axios.get(`${endpoint}/ApiRoutes/listaCD`)
+    //     console.log("Listando centros de distribucion");
+    //     setCentroDist(response.data)
+    // }
+
+    const ShowCentroDist = () => {
+        const [centroDist, setCentroDist] = useState([])
+        useEffect ( ()=> {
+            getAllCentroDist()
+        }, [])
+        const getAllCentroDist = async () => {
+            const response = await axios.get(`${endpoint}/listaCD`)
+            setCentroDist(response.data)
+        }       
+    }
+
     return (
         <div style={{ margin: 100 }}>
             <h2>Proyecto Semestral Desarrollo Web</h2>
             <h3>Martín Araneda - Manuel Yáñez</h3>
             <h1>Centros de Distribución</h1>
+            <h4>{ShowCentroDist()}</h4>
             <CDCard
                 name={"Centro de distribucion 1"}
                 description={"Probando descripcion"}
